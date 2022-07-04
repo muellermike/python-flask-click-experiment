73aa8de3f8a971677e548e2dc7aa6313c8c1f5f2
muellermike <mueller.doublem@gmail.com>
2022-07-04 10:15:13 +0200

    change db logic

--- datalayers/db.py
+++ datalayers/db.py
@@ -2,19 +2,18 @@ from distutils.command.config import config
 from flask import current_app
 import pymysql.cursors
 
+conn = None
+
 def execute(sql, params, statement):
     """
     Runs an sql command on the database and returns result if there is one.
     """
+    global conn
+
     result = None
 
-    conn = pymysql.connect(host = current_app.config["DB_URI"],
-                            port = 3306,
-                            user = current_app.config["DB_USER"],
-                            password = current_app.config["DB_PW"],
-                            db = current_app.config["DB_NAME"],
-                            charset = "utf8mb4",
-                            cursorclass = pymysql.cursors.DictCursor)
+    if (conn is None):
+        conn = set_db_connection()
 
     try:
         with conn.cursor() as cursor:
@@ -34,3 +33,12 @@ def execute(sql, params, statement):
         conn.close()
     
     return result
+
+def set_db_connection():
+    return pymysql.connect(host = current_app.config["DB_URI"],
+                            port = 3306,
+                            user = current_app.config["DB_USER"],
+                            password = current_app.config["DB_PW"],
+                            db = current_app.config["DB_NAME"],
+                            charset = "utf8mb4",
+                            cursorclass = pymysql.cursors.DictCursor)
